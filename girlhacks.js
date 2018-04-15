3966e50bd3fd24230847d2e19e79eb50d6c8bf8c
tsultana5226 <35276031+tsultana5226@users.noreply.github.com>
2018-04-15 17:02:44 -0400

    Create girlhacks.js

---
+++ girlhacks.js
@@ -0,0 +1,17 @@
+$('.card').click(function(){  
+ 
+  if (!$(this).hasClass("flipped")) {
+  $( ".face" ).addClass( 'off' );
+  $( this ).children( ".face" ).removeClass( 'off' );
+  $( this ).parent( ".cards" ).addClass( 'big' );
+  $( this ).addClass('flipped');
+
+  } else {
+ 
+  $( ".face" ).removeClass( 'off' );
+  $( ".cards" ).removeClass( 'big' );
+  $( this ).removeClass('flipped'); 
+}
+  
+
+});
