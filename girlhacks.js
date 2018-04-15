fd856306a141e9df472a9dc28fd15fd6d1ab6d3b
tsultana5226 <35276031+tsultana5226@users.noreply.github.com>
2018-04-15 17:37:58 -0400

    Update girlhacks.js

--- girlhacks.js
+++ girlhacks.js
@@ -1,16 +1,16 @@
-$('.card').click(function(){  
+('.card').click(function(){  
  
   if (!$(this).hasClass("flipped")) {
-  $( ".face" ).addClass( 'off' );
-  $( this ).children( ".face" ).removeClass( 'off' );
-  $( this ).parent( ".cards" ).addClass( 'big' );
-  $( this ).addClass('flipped');
+  ( ".face" ).addClass( 'off' );
+  ( this ).children( ".face" ).removeClass( 'off' );
+  ( this ).parent( ".cards" ).addClass( 'big' );
+  ( this ).addClass('flipped');
 
   } else {
  
-  $( ".face" ).removeClass( 'off' );
-  $( ".cards" ).removeClass( 'big' );
-  $( this ).removeClass('flipped'); 
+  ( ".face" ).removeClass( 'off' );
+  ( ".cards" ).removeClass( 'big' );
+  ( this ).removeClass('flipped'); 
 }
   
 
